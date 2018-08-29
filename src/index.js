af085ed5a6a161b3f230804b3059f023ed43b057
JohnDaise <jdaise@gmail.com>
2018-08-29 16:41:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ import App from './components/App';
 import './fetch-setup';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+let fetchFinder = {
+  'all': `/api/pets`,
+  'cat': `/api/pets?type=cat`,
+  'dog': `/api/pets?type=dog`,
+  'micropig': `/api/pets?type=micropig`
+}
+
+let onFindPetsClick = (query) => {
+    fetch(fetchFinder[query])
+      .then(r=> r.json())
+        .then(json =>
+          console.log(json)
+          // this.setState({
+          //   pets: json
+          // })
+        )}
